92742b29c314ee730f0d4bc37f25e96df6182e08
rpe4a <zapman2la@gmail.com>
2019-01-12 22:51:48 +0500

    add LoginForm

--- react-bookstore/src/App.js
+++ react-bookstore/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 
 const App = () => (
-  <div>
+  <Container>
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
-  </div>
+  </Container>
 );
 
 export default App;
